f70546058ee68ee81fc2d6e48b3a24e5dafc4daa
NSRdev <nahuelsanrod@gmail.com>
2020-10-08 11:39:53 +0100

    React Redux to load dishes

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Label, Col, Row, Modal, ModalHeader, ModalBody } from "reactstrap";
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 function RenderDish({dish}) {
     return(
@@ -19,7 +20,6 @@ function RenderDish({dish}) {
 
 
 function RenderComments({comments, addComment, dishId}) {
-    var res = <h4>Comments</h4>;
 
     if (comments != null) {
         let list = comments.map((comment) => {
@@ -47,7 +47,23 @@ function RenderComments({comments, addComment, dishId}) {
 
 
 const DishDetail = (props) => {
-    if (props.dish != null) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    } else if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    } else if (props.dish != null) {
         return (
             <div className="container">
                 <div className="row">
